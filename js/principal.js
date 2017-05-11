dd7e36d6395de9aecfdb41a061c0c75fbd14b121
Jefferson Verona <jefferson.verona@gmail.com>
2017-05-10 22:57:01 -0300

    Validações

--- js/principal.js
+++ js/principal.js
@@ -13,7 +13,30 @@ var nome = tdNome.textContent;
 var altura = tdAltura.textContent;
 var peso = tdPeso.textContent;
 var gordura = tdGordura.textContent;
-var imc = tdImc.textContent;
+var imc = peso / (altura * altura);
+
+//Tratando imc
+
+var alturaValida = true;
+var pesoValido = true;
+
+if (altura <= 0 || altura >= 3.0) {
+	tdImc.textContent = "Altura Inválida";
+	alturaValida = false;
+}
+
+if(peso <= 0 || peso >= 1000){
+	tdImc.textContent = "Peso Inválido";
+	pesoValido = false;
+}
+
+if(alturaValida == false && pesoValido == false){
+	tdImc.textContent = "Altura e peso são inválidos!"
+}
+
+if(alturaValida && pesoValido){
+	tdImc.textContent = imc;
+}
 
 //Exibindo Resultado
 console.log(nome);
